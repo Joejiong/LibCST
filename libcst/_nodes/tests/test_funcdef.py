d400ed3822e9ebebc825687b81ab3392d9de7d75
Tim Hatch <thatch@fb.com>
2019-11-10 07:27:33 -0800

    [parsing 3.3] async def (PEP 492)

--- libcst/_nodes/tests/test_funcdef.py
+++ libcst/_nodes/tests/test_funcdef.py
@@ -8,7 +8,7 @@ from typing import Any, Callable
 
 import libcst as cst
 from libcst import parse_statement
-from libcst._nodes.tests.base import CSTNodeTest, DummyIndentedBlock
+from libcst._nodes.tests.base import CSTNodeTest, DummyIndentedBlock, parse_statement_as
 from libcst.metadata import CodeRange
 from libcst.testing.utils import data_provider
 
@@ -1612,3 +1612,34 @@ class FunctionDefParserTest(CSTNodeTest):
     )
     def test_valid(self, node: cst.CSTNode, code: str) -> None:
         self.validate_node(node, code, parse_statement)
+
+    @data_provider(
+        (
+            {
+                "code": "async def foo(): pass",
+                "parser": parse_statement_as(python_version="3.7"),
+                "expect_success": True,
+            },
+            {
+                "code": "async def foo(): pass",
+                "parser": parse_statement_as(python_version="3.6"),
+                "expect_success": True,
+            },
+            {
+                "code": "async def foo(): pass",
+                "parser": parse_statement_as(python_version="3.5"),
+                "expect_success": True,
+            },
+            {
+                "code": "async def foo(): pass",
+                "parser": parse_statement_as(python_version="3.3"),
+                "expect_success": False,
+            },
+        )
+    )
+    def test_versions(self, code, parser, expect_success) -> None:
+        if not expect_success:
+            with self.assertRaises(cst.ParserSyntaxError):
+                parser(code)
+        else:
+            parser(code)
