18918c83a88aecedf4f7aee0e1954d71b5557ffb
Tim Hatch <thatch@fb.com>
2019-12-24 08:14:25 -0800

    noqa convert_argslist

--- libcst/_parser/conversions/params.py
+++ libcst/_parser/conversions/params.py
@@ -52,7 +52,9 @@ from libcst._parser.whitespace_parser import parse_parenthesizable_whitespace
     ),
     version="<=3.5",
 )
-def convert_argslist(config: ParserConfig, children: Sequence[Any]) -> Any:
+def convert_argslist(  # noqa: C901: too complex
+    config: ParserConfig, children: Sequence[Any]
+) -> Any:
     params: List[Param] = []
     seen_default: bool = False
     star_arg: Union[Param, ParamStar, MaybeSentinel] = MaybeSentinel.DEFAULT
